8aec8455a8209d3015521139ba17f3493553dba6
Kien Dolorfo <kiendolorfo@gmail.com>
2023-10-02 19:23:58 +0800

    Add StudentProfile GET api

--- students/views.py
+++ students/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import StudentProfile
+from .serializer import StudentProfileSerializer
 
 # Create your views here.
 
-#Custom login view for students
-class StudentLoginView(LoginView):
-    template_name = "students/login.html"
-    redirect_authenticated_user = True
+
+class StudentProfileViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = StudentProfile.objects.all()
+    serializer_class = StudentProfileSerializer
