d74dfdbc111b96f44bdd11df5cc5f5cf4f3aab50
Kien Dolorfo <kiendolorfo@gmail.com>
2023-10-03 16:54:29 +0800

    Update Serializer partial update fot put/patch request

--- students/serializer.py
+++ students/serializer.py
@@ -6,3 +6,4 @@ class StudentProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentProfile
         fields = "__all__"
+        read_only_fields = ("user",)
--- students/views.py
+++ students/views.py
@@ -13,9 +13,22 @@ class StudentProfileViewSet(viewsets.ModelViewSet):
     serializer_class = StudentProfileSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_serializer(self, *args, **kwargs):
+        if self.action in ['update', 'partial_update']:
+            return StudentProfileSerializer
+        return StudentProfileSerializer
+
     def create(self, request, *args, **kwargs):
         serializer = StudentProfileSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = StudentProfileSerializer(instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
