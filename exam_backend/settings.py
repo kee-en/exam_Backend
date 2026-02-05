b422bd8c14c0af156354afbec279e4e07f5f262b
Kien Dolorfo <37242789+kee-en@users.noreply.github.com>
2026-02-05 21:17:07 +0800

    Change database user from 'root' to 'dev'

--- exam_backend/settings.py
+++ exam_backend/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "examportaldb",
-        "USER": "root",
+        "USER": "dev",
         "PASSWORD": "kien1029",
         "HOST": "localhost",
         "PORT": "",
